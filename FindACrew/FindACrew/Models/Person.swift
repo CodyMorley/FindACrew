5ed3a84f01dc0b21f7d9a295aa196204ee116420
Cody Morley <codymorleyinc@gmail.com>
2020-04-06 11:00:17 -0700

    That went wayyy too quickly. Going to have to rewatch the wholle class and fix the mess + understand anything going on in the code.

---
+++ FindACrew/FindACrew/Models/Person.swift
@@ -0,0 +1,28 @@
+//
+//  Person.swift
+//  FindACrew
+//
+//  Created by Bling Morley on 4/6/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import Foundation
+
+struct Person: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case name, gender
+        case birthYear = "birth_year"
+    }
+    
+    let name: String
+    let gender: String
+    let birthYear: String
+}
+
+
+extension Person: Hashable {
+}
+
+struct PersonSearch: Decodable {
+    let results: [Person]
+}
---
+++ FindACrew/FindACrew/View Controllers/PersonSearchViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PersonSearchViewController.swift
+//  FindACrew
+//
+//  Created by Cody Morley on 4/6/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import UIKit
+
+class PersonSearchViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
