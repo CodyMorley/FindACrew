3347f4d9a6113feda594d7e4502d5107ec62bc16
Cody Morley <codymorleyinc@gmail.com>
2020-04-06 18:39:38 -0700

    Stuck on storyboard stack view. If I don't do it I won't learn it so i refuse to download a finished repo

---
+++ FindACrew/FindACrew/Views/PersonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PersonTableViewCell.swift
+//  FindACrew
+//
+//  Created by Bling Morley on 4/6/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
